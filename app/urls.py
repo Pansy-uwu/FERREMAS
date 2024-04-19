d1718e008b86706fb0940e3f424b29ff1b7b9e6f
Pansy <so.alfaro@duocuc.cl>
2024-04-19 17:52:44 -0400

    Arreglo 1

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),  # Agrega esta línea
+    path('index/', views.index, name='index'),
+    path('login/', views.login, name='login'),  
+    path('nosotros/', views.nosotros, name='nosotros'),  
+    path('producto1/', views.producto1, name='producto1'), 
+    path('producto2/', views.producto2, name='producto2'), 
+    path('producto3/', views.producto3, name='producto3'), 
+    path('productos/', views.productos, name='productos'), 
+    path('register/', views.register, name='register'), 
 ]
--- app/views.py
+++ app/views.py
@@ -6,3 +6,31 @@ from django.contrib import admin
 def index(request):
     context={}
     return render(request, 'index.html', context)
+
+def login(request):
+    context={}
+    return render(request, 'login.html', context)
+
+def nosotros(request):
+    context={}
+    return render(request, 'nosotros.html', context)
+
+def producto1(request):
+    context={}
+    return render(request, 'producto1.html', context)
+
+def producto2(request):
+    context={}
+    return render(request, 'producto2.html', context)
+
+def producto3(request):
+    context={}
+    return render(request, 'producto3.html', context)
+
+def productos(request):
+    context={}
+    return render(request, 'productos.html', context)
+
+def register(request):
+    context={}
+    return render(request, 'register.html', context)
