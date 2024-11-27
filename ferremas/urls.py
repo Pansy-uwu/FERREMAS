51fc7e43c61bef0d51a9d500682462078e2c59e9
Damian <pokecodigos@gmail.com>
2024-11-27 16:55:52 -0300

    render3

--- ferremas/urls.py
+++ ferremas/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from app import views 
 
 
 
@@ -23,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/',include('app.urls')),
     path('', include('django.contrib.auth.urls')),
+    path('', views.index, name='index'),  # Ruta principal
 
 ]
